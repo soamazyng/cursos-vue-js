9210221f49a792080c5f32e41f96eb0c373b6409
Jaqueline Benedicto <soamazing@gmail.com>
2018-08-07 13:22:51 -0300

    final

--- school_vue1/v1/js/app2.js
+++ school_vue1/v1/js/app2.js
@@ -6,11 +6,10 @@ var app = new Vue({
 		orderCol : 'id',
 		orderInverse : 1,
 		pagination: {
-			maxPage : 4,
+			maxPage : 3,
 			current : 1,
 			totalItems: 0,
-			totalPages : 5,
-			listNumbers : [],
+			totalPages : 0,			
 			listPagination: []
 		}
 	},
@@ -18,16 +17,32 @@ var app = new Vue({
 		filterOrderBy : function(e, col){
 			e.preventDefault();
 			this.orderCol = col;
-			this.orderInverse = this.orderInverse * -1;			
+			this.orderInverse = this.orderInverse * -1;
 		},
 		previous : function(e){
 			e.preventDefault();
+
+			if(this.pagination.current === 1){
+				return false;
+			}
+
+			this.pagination.current = this.pagination.current - 1;
+			this.books = this.pagination.listPagination[this.pagination.current - 1];
 		},
 		next : function(e){
 			e.preventDefault();
+
+			if(this.pagination.current === this.pagination.totalPages){
+				return false;
+			}
+
+			this.pagination.current = this.pagination.current + 1
+			this.books = this.pagination.listPagination[this.pagination.current - 1];
 		},
-		pagePagination : function(e, id){
-			e.preventDefault();
+		pagePagination : function(e, current){
+			e.preventDefault();			
+			this.books = this.pagination.listPagination[current];
+			this.pagination.current = current + 1;
 		},				
 	},
 	filters: {
@@ -42,7 +57,26 @@ var app = new Vue({
 
 		//$http é o método do vue - resource 
 		self.$http.get('./dataServer.json').then(function(response){
-			self.books = response.data;
+
+			self.pagination.totalItems = response.data.length;
+			self.pagination.totalPages = Math.ceil(response.data.length / self.pagination.maxPage);			
+
+			var aux = [];
+			for(var k in response.data){				
+				aux.push(response.data[k]);	
+							
+				if(aux.length === self.pagination.maxPage){				
+					self.pagination.listPagination.push(aux);
+					aux = [];
+				}
+			}
+
+			if(aux.length > 0){
+				self.pagination.listPagination.push(aux);
+			}			
+
+			self.books = self.pagination.listPagination[0];
+
 		});
 	}
 });
