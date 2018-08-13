f8aa3a714187a1c47d6063142e08d84fc9c5e629
Jaqueline Benedicto <soamazing@gmail.com>
2018-08-12 21:06:34 -0300

    manifest

--- school_vue2/webpack2-curso/webpack.config.js
+++ school_vue2/webpack2-curso/webpack.config.js
@@ -1,3 +1,6 @@
+var webpack = require('webpack'); // se você está usando plugins nativos de webpack
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var InlineManifestWebpack = require('inline-manifest-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
@@ -7,7 +10,21 @@ module.exports = {
 		vendor:['jquery', 'bootstrap']
 	},
 	plugins: [
-		new CleanWebpackPlugin(['dist'])
+		//este plugin remove as dependencias dos códigos js e insere todos em vendor.
+		//ex: index.js depende do jquery porém o app com a index.js não vai ter o jquery repetindo		
+		new webpack.optimize.CommonsChunkPlugin({
+			name: ['vendor', 'manifest']//'manifest' guarda os bundles que não tiveram mudança
+		}),
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'index.ejs'),
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeAttributeQuotes: true
+			},
+			chunksSortMode: 'dependency'			
+		})		
     ],
 	output: {
 		filename : '[name].[chunkhash].bundle.js',
