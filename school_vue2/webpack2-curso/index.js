eaa1848e0b07b597e32c6029faafc447f24ea820
Jaqueline Benedicto <soamazing@gmail.com>
2018-08-13 13:49:04 -0300

    fim webpack2

--- school_vue2/webpack2-curso/index.js
+++ school_vue2/webpack2-curso/index.js
@@ -1,5 +1,13 @@
-$ = require('jquery');
+//import test from './test';
+// System.import('./test').then(module => {
+// 	console.log(module)
+// 	module.default();
+// }, error => {
 
-$(document).ready(function(){
-	$('input:text').val('jQuery');
+// });
+require('./app.less');
+
+//test();
+$(document).ready(function () {
+    $('input:text').val('JQuery');
 });
--- school_vue2/webpack2-curso/webpack.config.js
+++ school_vue2/webpack2-curso/webpack.config.js
@@ -1,33 +1,73 @@
-var webpack = require('webpack'); // se você está usando plugins nativos de webpack
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var InlineManifestWebpack = require('inline-manifest-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const path = require('path');
+var InlineManifestWebpackPlugin = require('inline-manifest-webpack-plugin');
+var path = require('path');
 
 module.exports = {
-	entry: {
-		app: './index.js',
-		vendor:['jquery', 'bootstrap']
-	},
-	plugins: [
-		//este plugin remove as dependencias dos códigos js e insere todos em vendor.
-		//ex: index.js depende do jquery porém o app com a index.js não vai ter o jquery repetindo		
-		new webpack.optimize.CommonsChunkPlugin({
-			name: ['vendor', 'manifest']//'manifest' guarda os bundles que não tiveram mudança
-		}),
-		new CleanWebpackPlugin(['dist']),
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, 'index.ejs'),
-			minify: {
-				removeComments: true,
-				collapseWhitespace: true,
-				removeAttributeQuotes: true
-			},
-			chunksSortMode: 'dependency'			
-		})		
+    entry: {
+        app: './index.js',
+        vendor: ['jquery', 'bootstrap'],
+    },
+    output: {
+        filename: '[name].[chunkhash].bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.ejs'
+        }),
+        new InlineManifestWebpackPlugin({
+            name: 'webpackManifest'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true
+        }),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
     ],
-	output: {
-		filename : '[name].[chunkhash].bundle.js',
-		path: path.resolve(__dirname, 'dist')		
-	}
-}
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: [['es2015',{modules: false}]]
+                }
+            },
+            {
+                test: /\.less$/,
+                loaders: ['style-loader','css-loader','less-loader'] //loaders tem que ser olhado da direta para a esquerda
+            },
+            {
+                test: /\.(woff|woff2|ttf|svg|eot)$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 8080
+    },
+    watchOptions: {
+        aggregateTimeout: 300, //delay para utilizar o webserver e dá um tempo para o webpack compilar
+        ignored: /node_modules/
+    },
+    performance: {
+        hints: false
+    },
+    devtool: 'source-map'
+};
+
+/*webpack hash - id da compilacao do pacote
+ chunkhash - id da compilacao do pedaço | do modulo*/
+
+/*grafo - grafico */
