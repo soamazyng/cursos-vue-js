3402c0d94b1cdf19d6617745af661f7b0f627963
Jaqueline Benedicto <soamazing@gmail.com>
2018-08-21 18:42:16 -0300

    +1

---
+++ u_mastering_web_apps/starbase/starbase/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="app">
+		<h3>{{title}}</h3>
+	</div>
+</template>
+
+<script>
+	export default{
+		name : 'app', 
+		data() {
+			return{
+				title : 'Generate Your Team'
+			}
+		}
+	}
+</script>
+
