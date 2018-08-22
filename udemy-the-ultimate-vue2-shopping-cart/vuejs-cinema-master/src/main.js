f2094ae5db54186aa19ec5ee6813f4f657e91779
Jaqueline Benedicto <soamazing@gmail.com>
2018-08-22 19:00:14 -0300

    +

--- udemy-the-ultimate-vue2-shopping-cart/vuejs-cinema-master/src/main.js
+++ udemy-the-ultimate-vue2-shopping-cart/vuejs-cinema-master/src/main.js
@@ -27,17 +27,37 @@ new Vue({
 			data(){return{
 				genres
 			}},
+			methods: {
+				checkFilter(){
+					console.log('teste');
+				}
+			},
 			template: `
 			<div id="movie-filter">
 				<h2>Filter results</h2>
 				<div class="filter-group">
-					<check-filter v-for="genre in genres"></check-filter>
+					<check-filter v-for="genre in genres" :title="genre" v-on:check-filter="checkFilter"></check-filter>
 				</div>
 			</div>
 			`,
 			components : {
 				'check-filter' : {
-					template : `<div> Filter </div>`,
+					data(){
+						return {
+							checked : false							
+						}
+					},
+					methods:{
+						checkFilter(){
+							this.checked = !this.checked;
+							this.$emit('check-filter');
+						}
+					},
+					props: ['title'],
+					template : `<div :class="{'check-filter' : true, active: checked}" v-on:click="checkFilter"> 
+							<span class="checkbox"></span>
+							<span class="check-filter-title">{{title}}</span>
+							</div>`,
 				}
 			}
 		}
