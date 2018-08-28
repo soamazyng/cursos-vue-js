9ce2707b36d248194fbf3025415ce0f299d9b966
Jaqueline Benedicto <soamazing@gmail.com>
2018-08-27 22:46:07 -0300

    +

---
+++ udemy-the-ultimate-vue2-shopping-cart/vuejs-cinema-master/src/components/Overview.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="overview">
+		<div class="main">
+			<movie-list :genre="genre" :time="time" :movies="movies" :day="day"></movie-list>
+			<movie-filter></movie-filter>
+		</div>
+	</div>
+</template>
+
+<script>
+	import MovieList from './MovieList.vue';
+	import MovieFilter from './MovieFilter.vue';
+
+	export default {  
+		props: ['genre', 'time', 'movies', 'day'],
+		data () {
+			return {
+
+			}
+		},
+		components: {
+			MovieList,	
+			MovieFilter
+		},
+	}
+</script>
+
+<style lang="css" scoped>
+</style>
