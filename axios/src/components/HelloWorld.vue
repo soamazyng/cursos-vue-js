6b0508e854a4476a9987952cf59a896610341c88
Jaqueline Benedicto <soamazing@gmail.com>
2018-11-30 14:16:57 -0200

    +

--- axios/src/components/HelloWorld.vue
+++ axios/src/components/HelloWorld.vue
@@ -23,7 +23,7 @@ export default {
     //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vcG9ydGFscmguc3Vuc2V0Y29tLmNvbS5ici9hcGkvbG9naW4iLCJpYXQiOjE1NDAzMDA5ODMsImV4cCI6MTU3MTgzNjk4MywibmJmIjoxNTQwMzAwOTgzLCJqdGkiOiJzZEp3S2RTUGtGTkVodm9YIiwic3ViIjoxLCJwcnYiOiIwYjBjZjUwYWYxMjNkODUwNmUxNmViYTdjYjY3NjI5NzRkYTNhYzNhIiwiaWQiOjEsInRvb2xzX2lkIjoyNSwicHJpbWVpcm9fbm9tZSI6IkphcXVlbGluZSIsInNlZ3VuZG9fbm9tZSI6IlRlaXhlaXJhIEJlbmVkaWN0byIsImVtYWlsIjoiamFxdWVsaW5lLnRlaXhlaXJhQHN1bnNldGNvbS5jb20uYnIiLCJhdmF0YXIiOiJlMDdjNzk1My03ZDg1LTQxNTktOGRmNC0wOTBlNjhmMDE1NmMuanBnIiwibGFzdF9hY3RpdmUiOiIyMDE4LTEwLTExIDE4OjQ2OjU3IiwiY3JlYXRlZF9hdCI6eyJkYXRlIjoiMjAxOC0xMC0xMSAyMTo0Nzo0NS4wMDAwMDAiLCJ0aW1lem9uZV90eXBlIjozLCJ0aW1lem9uZSI6IlVUQyJ9LCJ1cGRhdGVkX2F0Ijp7ImRhdGUiOiIyMDE4LTEwLTIyIDEzOjU1OjA3LjAwMDAwMCIsInRpbWV6b25lX3R5cGUiOjMsInRpbWV6b25lIjoiVVRDIn19.jCdP9opiOfkBEsqXLP38eafY3jbfP3bbZ_Y93oS8OQQ"
     // );    
 
-    // const current_time = Date.now() / 1000;    
+     const current_time = Date.now() / 1000;    
 
     // if (jwtDecode.exp < current_time) {
     //   console.log("data expirada");
